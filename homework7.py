5a490415cdab02b558a368d741d98afb4914eb29
Shushan39 <168975723+Shushan39@users.noreply.github.com>
2024-05-19 19:38:26 +1000

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,7 @@
+def print_params(animal_type, pet_name):
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+print_params('hamster', 'harry')
+print_params('dog', 'wille')
