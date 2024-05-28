47d92efe8a9428ae6ab5a9d3710ef89398f93cda
Shushan39 <168975723+Shushan39@users.noreply.github.com>
2024-05-28 23:14:26 +1000

    Update homework9.py

--- homework9.py
+++ homework9.py
@@ -1,9 +1,11 @@
-n = int(input('Введите число от 3 до 20'))
-for i in range(1, n - 1):
+n = int(input('Введите число от 3-х до 20-ти: '))
+result = str()
+a = int(n)
+for i in range(1, n - 1):  # Вычисление пароля
     for j in range(i + 1, n):
-        c = i + j
-        if n % c == 0:
-            print(i, j)
+        if n % (i + j) == 0:
+            result = result + str(i) + str(j)
+print(a, '-', result)
 
 
 
